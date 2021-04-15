f8e718b23936ceeed74f689e8e864924f68b126b
bgvictor <victor.boga@hotmail.com>
2021-04-15 10:44:43 +0200

    (CORRECTO) Layouts realizados (excepto los elementos de los List View).

---
+++ app/src/main/java/com/indytek/pufsmanagement/Productos.java
@@ -0,0 +1,16 @@
+package com.indytek.pufsmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.indytek.pufsmanagement.R;
+
+public class Productos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_productos);
+    }
+}
